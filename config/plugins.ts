4080ec7bc97e53827519c7d9b8fb412912e1d481
whiletrue.io <contact@whiletrue.io>
2025-05-22 13:18:30 +0200

    chore: add users-permissions jwtSecret config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+import { StrapiUserPermissionsPlugin } from '@strapi/strapi'; // se hai i tipi
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+} as StrapiUserPermissionsPlugin);
+
