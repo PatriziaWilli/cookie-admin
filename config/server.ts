5cb11180e2a02c0c27777db4a0f5ddb46addeaae
whiletrue.io <contact@whiletrue.io>
2025-05-22 13:03:53 +0200

    chore: provide default app.keys and read APP_KEYS from env

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,11 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // In locale usiamo due chiavi di default, in produzione
+    // Railway passerà APP_KEYS via env (virgola-separate)
+    keys: env.array('APP_KEYS', [
+      'devKey1_changeMe', 
+      'devKey2_changeMe'
+    ]),
   },
 });
